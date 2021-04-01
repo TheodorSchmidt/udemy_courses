46ac9de214b4e65b836dee28c3360eceada44277
Theodor Schmidt <bogdan_zakharov_99@mail.ru>
2021-04-01 20:13:30 +0400

    Do 2 task

--- script.js
+++ script.js
@@ -8,11 +8,23 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt("Один из последних просмотренных фильмов?", ''),
-    b = +prompt("На сколько оцените его?", ''),
-    c = prompt("Один из последних просмотренных фильмов?", ''),
-    d = +prompt("На сколько оцените его?", '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+let a, b;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько вы его оцените?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 console.log(personalMovieDB);
