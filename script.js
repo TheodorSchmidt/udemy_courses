69950ac2b90324e42e382df90f604f4c447c7add
Theodor Schmidt <bogdan_zakharov_99@mail.ru>
2021-04-01 17:53:19 +0400

    do 2nd point

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 "use strict";
 //создать переменную и поместить в нее ответ от пользователя 
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {}, 
+    actors: {},
+    genres: [],
+    privat: false
+};
