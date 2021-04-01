cfb3c25556dbcc49e94261964bd5b3025c37bf28
Theodor Schmidt <bogdan_zakharov_99@mail.ru>
2021-04-01 18:05:04 +0400

    refactoring task

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 "use strict";
 //создать переменную и поместить в нее ответ от пользователя 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-let personalMovieDB = {
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {}, 
     actors: {},
     genres: [],
     privat: false
 };
-let a = prompt("Один из последних просмотренных фильмов?", ''),
-    b = prompt("На сколько оцените его?", ''),
+const a = prompt("Один из последних просмотренных фильмов?", ''),
+    b = +prompt("На сколько оцените его?", ''),
     c = prompt("Один из последних просмотренных фильмов?", ''),
-    d = prompt("На сколько оцените его?", '');
+    d = +prompt("На сколько оцените его?", '');
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
